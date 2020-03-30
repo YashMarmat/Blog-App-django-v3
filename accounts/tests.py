3af0a55e22b333fa2c6bfab833963c8814776c10
Yash Marmat <yashmarmat08@gmail.com>
2020-03-30 20:37:01 +0530

    Update tests.py

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class SignUpPageTest(TestCase):
+
+    username = 'newuser'
+    email = 'newuser@gmail.com'
+
+    def test_signup_page_status_code(self):
+        response = self.client.get('/accounts/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_signup_url_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_signup_uses_correct_template(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'signup.html')
+
+    def test_signup_form(self):
+        new_user = get_user_model().objects.create_user(self.username, self.email)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
+
+
