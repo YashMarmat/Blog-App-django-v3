47de00daa4b11b62e8e4ebc5ea1225129fa92d00
Yash Marmat <yashmarmat08@gmail.com>
2020-03-30 19:30:24 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -11,7 +11,7 @@ from .views import(
 urlpatterns = [
     path('', BlogList.as_view(), name = "blog_list"),
     path('<int:pk>', BlogDetail.as_view(), name = "blog_detail"),
-    path('blog/new/', BlogCreate.as_view(), name = 'blog_new'), 
-    path('blog/<int:pk>/update', BlogUpdate.as_view(), name = 'blog_update'),
-    path('blog/<int:pk>/delete', BlogDelete.as_view(), name = 'blog_delete'),
+    path('new/', BlogCreate.as_view(), name = 'blog_new'), 
+    path('<int:pk>/update', BlogUpdate.as_view(), name = 'blog_update'),
+    path('<int:pk>/delete', BlogDelete.as_view(), name = 'blog_delete'),
 ]
